22d0814dd9bc3e5915d896d10b44d3bef28f75a9
Matias Piipari <matias.piipari@gmail.com>
2024-02-11 16:57:28 +0200

    Reflect most important configurability in the launcher and install hook for defaults

--- bin/snap_launcher.sh
+++ bin/snap_launcher.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 set -e
-#. $SNAP/backend/venv/bin/activate
+
+DATA_DIR=$(snapctl get data-dir) \
+PORT=$(snapctl get port) \
+HOST=$(snapctl get host) \
+WEBUI_SECRET_KEY=$(snapctl get secret-key) \
+OLLAMA_API_BASE_URL=$(snapctl get ollama-api-base-url) \
+OPENAI_API_BASE_URL=$(snapctl get openai-api-base-url) \
+OPENAI_API_KEY=$(snapctl get openai-api-key) \
+ENABLE_SIGNUP=$(snapctl get enable-signup) \
 $SNAP/backend/start.sh $@
